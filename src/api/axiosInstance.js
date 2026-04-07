cd5a15c7f13f112ee1d43a0236322691f40cb6de
vigorous <ajeet.vigorousit@gmail.com>
2026-04-07 15:15:57 +0530

    save new chnages

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://astrology-i7c9.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://astrology-i7c9.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
